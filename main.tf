8414aaf98908c3bc3b4f0725d7a25ff42f047135
MeathSpace <40881591+MeathSpace@users.noreply.github.com>
2024-07-09 18:05:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,29 @@
-resource "aws_instance" "my_vm" {
- ami           = var.ami //Ubuntu AMI
- instance_type = var.instance_type
-
- tags = {
-   Name = var.name_tag,
- }
+terraform {
+  # Required Terraform Version
+  required_version = ">= 1.6.5"
+  # Required Providers and their Versions
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.30"
+    }
+  }
 }
-
+ 
+# Settig up AWS Region & Auth
+provider "aws" {
+#   region  = var.deployment_regions[0]
+region  = "eu-north-1"
+#   profile = var.aws_profile
+profile = "default"
+  default_tags {
+    tags = {
+      "adv/Project"   = "SHIP"
+      "adv/Component" = "DataLakeHouse"
+      "adv/Stage"     = terraform.workspace
+    #   "access-team"   = var.access_team
+    #   "cost-center"   = var.cost_center
+    }
+  }
+}
+ 
