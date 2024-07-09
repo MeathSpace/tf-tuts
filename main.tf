09c87e3f2314d67c22367b1269722b43611a94d9
MeathSpace <40881591+MeathSpace@users.noreply.github.com>
2024-07-09 18:09:56 +0530

    Sample for terraform poc

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 terraform {
-  # Required Terraform Version
   required_version = ">= 1.6.5"
-  # Required Providers and their Versions
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,20 +8,12 @@ terraform {
   }
 }
  
-# Settig up AWS Region & Auth
 provider "aws" {
-#   region  = var.deployment_regions[0]
 region  = "eu-north-1"
-#   profile = var.aws_profile
 profile = "default"
-  default_tags {
-    tags = {
-      "adv/Project"   = "SHIP"
-      "adv/Component" = "DataLakeHouse"
-      "adv/Stage"     = terraform.workspace
-    #   "access-team"   = var.access_team
-    #   "cost-center"   = var.cost_center
-    }
-  }
+}
+
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "terraform_sample_jenkins"
 }
  
