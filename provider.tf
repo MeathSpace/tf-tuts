2c5dc9b970191947ab3599aff8ec6dec19377ee4
MeathSpace <40881591+MeathSpace@users.noreply.github.com>
2024-07-09 18:06:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,1 @@
-terraform {
- required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "~> 4.18.0"
-   }
- }
-
- backend "s3" {
-   bucket = "tf-tuts-state-1"
-   key    = "state"
-   region = "eu-central-1"
- }
-}
 
