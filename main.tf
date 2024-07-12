a2e20ba262a7668a5c7de1dd11b57d3304753d35
MeathSpace <40881591+MeathSpace@users.noreply.github.com>
2024-07-12 14:38:48 +0530

    added Lambda function code and terraform script

--- main.tf
+++ main.tf
@@ -19,3 +19,62 @@ resource "aws_s3_bucket" "my_s3_bucket" {
   bucket = "samplejenkinstest"
 }
  
+
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Effect = "Allow",
+        Sid    = ""
+      }
+    ]
+  })
+
+  inline_policy {
+    name   = "lambda_policy"
+    policy = jsonencode({
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Action = [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          Effect   = "Allow",
+          Resource = "arn:aws:logs:*:*:*"
+        }
+      ]
+    })
+  }
+}
+
+resource "aws_lambda_function" "hello_world_lambda" {
+  function_name = "hello_world_lambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+  filename      = "lambda_function.zip"
+
+  source_code_hash = filebase64sha256("lambda_function.zip")
+
+  # environment {
+  #   variables = {
+  #     # Add any environment variables here
+  #   }
+  # }
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_world_lambda.function_name
+  principal     = "events.amazonaws.com"
+}
