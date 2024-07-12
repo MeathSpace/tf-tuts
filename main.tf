17585a790fecde32c25b1d23347cda993a0caf4c
MeathSpace <40881591+MeathSpace@users.noreply.github.com>
2024-07-12 17:59:27 +0530

    undo lasat commit

--- main.tf
+++ main.tf
@@ -91,23 +91,23 @@ resource "aws_apigatewayv2_stage" "lambda" {
   name        = "serverless_lambda_stage"
   auto_deploy = true
 
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+  # access_log_settings {
+  #   destination_arn = aws_cloudwatch_log_group.api_gw.arn
 
-    format = jsonencode({
-      requestId               = "$context.requestId"
-      sourceIp                = "$context.identity.sourceIp"
-      requestTime             = "$context.requestTime"
-      protocol                = "$context.protocol"
-      httpMethod              = "$context.httpMethod"
-      resourcePath            = "$context.resourcePath"
-      routeKey                = "$context.routeKey"
-      status                  = "$context.status"
-      responseLength          = "$context.responseLength"
-      integrationErrorMessage = "$context.integrationErrorMessage"
-      }
-    )
-  }
+  #   format = jsonencode({
+  #     requestId               = "$context.requestId"
+  #     sourceIp                = "$context.identity.sourceIp"
+  #     requestTime             = "$context.requestTime"
+  #     protocol                = "$context.protocol"
+  #     httpMethod              = "$context.httpMethod"
+  #     resourcePath            = "$context.resourcePath"
+  #     routeKey                = "$context.routeKey"
+  #     status                  = "$context.status"
+  #     responseLength          = "$context.responseLength"
+  #     integrationErrorMessage = "$context.integrationErrorMessage"
+  #     }
+  #   )
+  # }
 }
 
 resource "aws_apigatewayv2_integration" "hello_world_lambda" {
@@ -125,11 +125,11 @@ resource "aws_apigatewayv2_route" "hello_world_lambda" {
   target    = "integrations/${aws_apigatewayv2_integration.hello_world_lambda.id}"
 }
 
-resource "aws_cloudwatch_log_group" "api_gw" {
-  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+# resource "aws_cloudwatch_log_group" "api_gw" {
+#   name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
 
-  retention_in_days = 30
-}
+#   retention_in_days = 30
+# }
 
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
