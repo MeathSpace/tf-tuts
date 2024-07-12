e3c2075e638488987ce9d501ad120719d5b826a2
MeathSpace <40881591+MeathSpace@users.noreply.github.com>
2024-07-12 14:42:17 +0530

    commented back up files

--- backUp.tf
+++ backUp.tf
@@ -1,21 +1,21 @@
-terraform {
-  required_version = ">= 1.6.5"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.30"
-    }
-  }
-}
+# terraform {
+#   required_version = ">= 1.6.5"
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.30"
+#     }
+#   }
+# }
  
-provider "aws" {
-region  = "eu-north-1"
-profile = "sumit"
-shared_config_files      = [ "~/.aws/config"] 
-shared_credentials_files = [ "~/.aws/credentials"] 
-}
+# provider "aws" {
+# region  = "eu-north-1"
+# profile = "sumit"
+# shared_config_files      = [ "~/.aws/config"] 
+# shared_credentials_files = [ "~/.aws/credentials"] 
+# }
 
-resource "aws_s3_bucket" "my_s3_bucket" {
-  bucket = "samplejenkinstest"
-}
+# resource "aws_s3_bucket" "my_s3_bucket" {
+#   bucket = "samplejenkinstest"
+# }
  
