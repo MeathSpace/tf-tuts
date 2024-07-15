902a4ea9fab1e4268de988678d453144834794c9
MeathSpace <40881591+MeathSpace@users.noreply.github.com>
2024-07-16 01:13:31 +0530

    Test2

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ def lambda_handler(event, context):
 
     response = {
         "statusCode" : 200,
-        "body" : "Perfect Response Reloading"
+        "body" : "Perfect Response Reloading Check"
 
     }
     return response
