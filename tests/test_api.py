fc9b5bbe0e7a964cee4a183c7f049fa6fdeb910e
MeathSpace <40881591+MeathSpace@users.noreply.github.com>
2024-07-16 01:32:59 +0530

    modified tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,4 +5,5 @@ import allure
 def test_apirequest():
     resp = requests.request(url="https://u9tnio6ui4.execute-api.eu-north-1.amazonaws.com/serverless_lambda_stage/hello", method="GET")
     print(resp.json)
-    assert int(resp.status_code) == 200
+    assert int(resp.statusCode) == 200
+     assert int(resp.body.sum) == 30
