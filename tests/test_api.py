183b3c991100b6b5539ee76cd861ee2666a22134
MeathSpace <40881591+MeathSpace@users.noreply.github.com>
2024-07-16 01:48:32 +0530

    undo tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,16 +1,26 @@
+# import requests
+# import allure
+# import json
+
+# @allure.description("API Test")
+# def test_apirequest():
+#     resp = requests.get(url="https://u9tnio6ui4.execute-api.eu-north-1.amazonaws.com/serverless_lambda_stage/hello")
+#     response_json = resp.json()
+#     print(response_json)
+    
+#     # Check statusCode
+#     assert response_json['statusCode'] == 200
+    
+#     # Check sum in the body
+#     body = response_json['body']
+#     assert body['sum'] == 30
+
+
 import requests
 import allure
-import json
-
-@allure.description("API Test")
+ 
+@allure.description("Api Test")
 def test_apirequest():
-    resp = requests.get(url="https://u9tnio6ui4.execute-api.eu-north-1.amazonaws.com/serverless_lambda_stage/hello")
-    response_json = resp.json()
-    print(response_json)
-    
-    # Check statusCode
-    assert response_json['statusCode'] == 200
-    
-    # Check sum in the body
-    body = response_json['body']
-    assert body['sum'] == 30
+    resp = requests.request(url="https://google.com", method="GET")
+    print(resp.json)
+    assert int(resp.status_code) == 200
